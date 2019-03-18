14b9fbc0f587f8df8f3f16123d7fc0c1a104e358
yuganshu <yuganshunda@gmail.com>
2019-03-18 21:07:39 +0530

    changed with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
